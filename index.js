ef2f16b7f1cc3072117fc61aeff9bcaa174677d0
matthew lee <matthewlee07@gmail.com>
2018-06-19 01:01:46 -0400

    original...should be working...

--- index.js
+++ index.js
@@ -41,17 +41,3 @@ $(document).ready(function () {
         ]
     });
 });
-
-let acc = document.getElementsByClassName("accordion");
-
-for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
