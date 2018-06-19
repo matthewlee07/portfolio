65c73c483353718e69b88c988fa13265c7feb83d
matthew lee <matthewlee07@gmail.com>
2018-06-19 00:54:33 -0400

    problem with accordion

---
+++ index.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    let autoplaySpeed;
+    if ($(window).width() > 739) {
+        autoplaySpeed = 5000;
+    }
+    else {
+        autoplaySpeed = 1500;
+    }
+
+    $('.photo_carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+});
+
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
